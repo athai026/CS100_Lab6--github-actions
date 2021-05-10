1a17ec6fd6e28fd7416905eee4abbaede00088d9
alice-thaitea <athai026@ucr.edu>
2021-05-09 23:38:04 -0700

    Constructor unit test: Rectangle(3, 3)

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -3,8 +3,19 @@
 Rectangle::Rectangle() {}
 
 Rectangle::Rectangle(int w, int h) {
-    width = w;
-    height = h;
+    if (w > 0) {
+        width = w;
+    }
+    else {
+        throw "Cannot have negative or zero width";
+    }
+
+    if (h > 0) {
+        height = h;
+    }
+    else {
+        throw "Cannot have negative or zero height";
+    }
 }
 
 void Rectangle::set_width(int w) {
@@ -13,6 +24,24 @@ void Rectangle::set_width(int w) {
 
 void Rectangle::set_height(int h) {
     this->height = h;
+}
+
+int Rectangle::get_width() {
+    if (width > 0) {
+        return this->width;
+    }
+    else {
+        throw "Cannot have negative or zero width";
+    }
+}
+
+int Rectangle::get_height() {
+    if (height > 0) {
+        return this->height;
+    }
+    else {
+        throw "Cannot have negative or zero height";
+    }
 }
 
 int Rectangle::area() {
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,3 +1,5 @@
+#include "rectangle_test.hpp"
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
