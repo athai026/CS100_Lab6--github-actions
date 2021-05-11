eff0eb2c3bc25e950413dc0d65aa0067399cb713
AEssaian740 <aessaian101@gmail.com>
2021-05-10 22:57:21 -0700

    Added rectangle_test.hpp

---
+++ tests/rectangle_test.hpp
@@ -0,0 +1,13 @@
+#ifndef __RECTANGLE_TEST_HPP__
+#define __RECTANGLE_TEST_HPP__
+
+#include "gtest/gtest.h"
+#include "../header/rectangle.hpp"
+
+TEST(Constructor, width3_height3) {
+    Rectangle* rect1 = new Rectangle(3, 3);
+    EXPECT_EQ(rect1->get_width(), 3);
+    EXPECT_EQ(rect1->get_height(), 3);
+}
+
+#endif //__RECTANGLE_TEST_HPP__
